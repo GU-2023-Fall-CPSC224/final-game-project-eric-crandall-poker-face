2c7d27768fdc3688a5915e98e2e934c4d28d7ccc
Jacrispys <jvanzyverden@icloud.com>
2023-11-01 13:09:02 -0700

    Hydra :)

---
+++ src/main/java/edu/gonzaga/HydraListener.java
@@ -0,0 +1,47 @@
+package edu.gonzaga;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Random;
+
+public class HydraListener extends WindowAdapter {
+
+    private int hydraCounter = 0;
+
+    public static HydraListener INSTANCE = null;
+
+    public static HydraListener getInstance() {
+        return INSTANCE != null ? INSTANCE : new HydraListener();
+    }
+
+    private HydraListener() {
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        super.windowClosed(e);
+        JFrame hydra1 = new JFrame("Hydra " + ++hydraCounter);
+        JFrame hydra2 = new JFrame("Hydra " + ++hydraCounter);
+        hydra1.setSize(640, 360);
+        hydra2.setSize(640, 360);
+        Random rand = new Random();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int xLoc = dim.width/2-hydra1.getSize().width/2;
+        int yLoc = dim.height/2-hydra1.getSize().height/2;
+        int rand1 = rand.nextInt(600) - 300;
+        int rand2 = rand.nextInt(600) - 300;
+        int rand3 = rand.nextInt(600) - 300;
+        int rand4 = rand.nextInt(600) - 300;
+        hydra1.setLocation(rand1+ xLoc, rand2 + yLoc);
+        hydra2.setLocation(rand3 + xLoc, rand4 + yLoc);
+        hydra1.setVisible(true);
+        hydra2.setVisible(true);
+        hydra1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        hydra2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        hydra1.addWindowListener(this);
+        hydra2.addWindowListener(this);
+    }
+}
--- src/main/java/edu/gonzaga/MainGame.java
+++ src/main/java/edu/gonzaga/MainGame.java
@@ -13,11 +13,28 @@
 package edu.gonzaga;
 
 
+import javax.swing.*;
+import java.awt.*;
+
 /** Main program class for launching your team's program. */
 public class MainGame {
+
+    public static boolean hydra = false;
+
     public static void main(String[] args) {
         System.out.println("Hello Team Game");
+        JFrame frame1 = new JFrame("Hydra?");
+        frame1.setSize(640, 360);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame1.setLocation(dim.width/2-frame1.getSize().width/2, dim.height/2-frame1.getSize().height/2);
+        if (hydra) {
+            frame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame1.addWindowListener(HydraListener.getInstance());
+        } else {
+            frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
 
+        frame1.setVisible(true);
 
         // Your code here. Good luck!
     }
