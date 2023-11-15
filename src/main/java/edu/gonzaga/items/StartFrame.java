f355b6df428c7faed6f99ab1c01ae4087d97a848
gabeh135 <gabeh135@gmail.com>
2023-11-15 11:24:44 -0800

    implement players and rounds input

--- src/main/java/edu/gonzaga/items/StartFrame.java
+++ src/main/java/edu/gonzaga/items/StartFrame.java
@@ -9,10 +9,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class StartFrame {
-
+    private Integer numPlayers;
+    private Integer numRounds;
+    private final Integer DEFAULT_NUM_PLAYERS = 2;
+    private final Integer DEFAULT_NUM_ROUNDS = 5;
     private final JFrame frame;
 
     public StartFrame() {
+        numPlayers = DEFAULT_NUM_PLAYERS;
+        numRounds = DEFAULT_NUM_ROUNDS;
         frame = new JFrame("Eric Crandall Poker");
         initFrame(frame);
     }
@@ -21,10 +26,38 @@ public class StartFrame {
         return this.frame;
     }
 
-    // TODO: 11/13/2023 Pull object out of method and into variables with getters and setters for better accessibility. 
+    public void updateNumPlayers(Integer playersValue) {
+        // TODO: add upper bounds
+        if (playersValue > DEFAULT_NUM_PLAYERS) {
+            this.numPlayers = playersValue;
+        }
+        System.out.println(numPlayers);
+    }
+
+    public void updateNumRounds(Integer roundsValue) {
+        // TODO: add upper bounds
+        if (roundsValue > 0) {
+            this.numRounds = roundsValue;
+        }
+        System.out.println(numRounds);
+    }
+
+    public Integer getIntegerValueOfInput(String s) {
+        try {
+            Integer i = Integer.valueOf(s);
+            return i;
+        }
+
+        catch (NumberFormatException er) {
+            return -1;
+        }
+    }
+
+    // TODO: 11/13/2023 Pull object out of method and into variables with getters
+    // and setters for better accessibility.
 
     private void initFrame(JFrame frame) {
-        //don't allow resize
+        // don't allow resize
         frame.setSize(520, 360);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
@@ -35,24 +68,23 @@ public class StartFrame {
         } else {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         }
-        
-        //north panel
+
+        // north panel
         JPanel northPanel = new JPanel();
         JLabel word = new JLabel("Eric Crandall Poker");
 
         northPanel.add(word);
 
-
-        //south panel
+        // south panel
         JPanel southPanel = new JPanel(new BorderLayout());
 
         JButton soundButton = new JButton("Sound");
         JButton playButton = new JButton("Play Game");
 
-        JLabel playerLabel = new JLabel("Players: ");
-        JTextField playerField = new JTextField(1);
+        JLabel playersLabel = new JLabel("Players: ");
+        JTextField playersField = new JTextField(1);
 
-        //make hidden if playing until bust
+        // make hidden if playing until bust
         JLabel roundsLabel = new JLabel("Rounds: ");
         JTextField roundsField = new JTextField(1);
 
@@ -66,37 +98,34 @@ public class StartFrame {
         settingsMenu.add(bustMode);
         settingsBar.add(settingsMenu);
 
-        //panel for content set in center of south panel
+        // panel for content set in center of south panel
         JPanel southPanel2 = new JPanel();
 
-        //make playerLabel and playerField part of own panel
-        southPanel2.add(playerLabel);
-        southPanel2.add(playerField);
+        // make playerLabel and playerField part of own panel
+        southPanel2.add(playersLabel);
+        southPanel2.add(playersField);
 
-        //make roundsLabel and roundsField part of own panel
+        // make roundsLabel and roundsField part of own panel
         southPanel2.add(roundsLabel);
         southPanel2.add(roundsField);
         southPanel2.add(settingsBar);
 
-        //soundButton and playButton should have space between seld and border
+        // soundButton and playButton should have space between seld and border
         southPanel.add(BorderLayout.WEST, soundButton);
         southPanel.add(BorderLayout.CENTER, southPanel2);
         southPanel.add(BorderLayout.EAST, playButton);
 
-
-        //placeholder button for center
+        // placeholder button for center
         JLabel centerLabel = new JLabel("placeholder");
 
-
-        //add panels to frame
+        // add panels to frame
         frame.getContentPane().add(BorderLayout.NORTH, northPanel);
         frame.getContentPane().add(BorderLayout.SOUTH, southPanel);
         frame.getContentPane().add(BorderLayout.CENTER, centerLabel);
 
         frame.setVisible(true);
 
-
-        //Testing muting sound
+        // Testing muting sound
         soundButton.addActionListener(ae -> {
             SoundThread sound = SoundThread.getInstance();
             if (sound.isPlaying()) {
@@ -106,15 +135,15 @@ public class StartFrame {
             }
         });
 
-        //when clicking playButton go to the player specify page
-        //          -> should this be its own frame or should we just replace the components of the start menu
-        //Testing taking in input
+        // when clicking playButton go to the player specify page
+        // -> should this be its own frame or should we just replace the components of
+        // the start menu
         playButton.addActionListener(ae -> {
-            String playerValue = playerField.getText();
-            System.out.println(playerValue + " Players");
+            String playersInput = playersField.getText();
+            updateNumPlayers(getIntegerValueOfInput(playersInput));
 
-            String roundsValue = roundsField.getText();
-            System.out.println(roundsValue + " Rounds");
+            String roundsInput = roundsField.getText();
+            updateNumRounds(getIntegerValueOfInput(roundsInput));
         });
     }
 }
