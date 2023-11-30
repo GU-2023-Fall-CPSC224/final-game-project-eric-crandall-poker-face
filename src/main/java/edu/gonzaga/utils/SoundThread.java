a2c385f9c99d299554c18bbeeb29568c189b926f
Jacrispys <jvanzyverden@icloud.com>
2023-11-30 11:22:59 -0800

    fixed sound thread

--- src/main/java/edu/gonzaga/utils/SoundThread.java
+++ src/main/java/edu/gonzaga/utils/SoundThread.java
@@ -1,8 +1,14 @@
 package edu.gonzaga.utils;
 
 import javax.sound.sampled.*;
+import javax.swing.text.html.HTMLDocument;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SoundThread extends Thread {
 
@@ -13,13 +19,14 @@ public class SoundThread extends Thread {
     }
 
     private final File file = new File("src/main/resources/audio/PokerFace.wav");
-    private final Clip clip;
+    private Clip clip;
     private final AudioInputStream ais;
 
     private SoundThread() {
         try {
             this.clip = AudioSystem.getClip();
             this.ais = AudioSystem.getAudioInputStream(file);
+            INSTANCE = this;
         } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
             throw new RuntimeException(e);
         } finally {
@@ -41,9 +48,28 @@ public class SoundThread extends Thread {
 
 
     public void startSong() {
-        if (this.clip.isOpen() && !this.clip.isRunning()) {
-            this.clip.start();
+        Future<Clip> song = startSongAsync(this.clip);
+        try {
+            this.clip = song.get();
+            if (!this.clip.isRunning()) {
+                this.clip.start();
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            Logger.getLogger(getClass().getSimpleName()).log(Level.SEVERE, "There was an error while starting song!");
+            throw new RuntimeException(e);
         }
+    }
+
+    private CompletableFuture<Clip> startSongAsync(Clip clip) {
+        CompletableFuture<Clip> cf = new CompletableFuture<>();
+        try {
+            while (!(this.clip.isOpen() && !this.clip.isRunning())) {
+                Thread.sleep(1000L);
+            }
+        } catch (InterruptedException ignored) {
+        }
+        cf.complete(clip);
+        return cf;
     }
 
     public void stopSong() {
